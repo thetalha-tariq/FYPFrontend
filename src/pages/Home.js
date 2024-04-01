bb27ec4353a0cf40d81ae768915b4688d8959a79
Talha Tariq <talhlacricket936@gmail.com>
2024-04-01 16:22:59 +0500

    Change Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div>This is Home For My Pet Mediconnect app project</div>;
+  return <div>This is Home For My Pet Medi app project</div>;
 };
 
 export default Home;
