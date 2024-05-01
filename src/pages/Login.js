2890b6610ce2a4c8e665d09efa4a3cd134504dbe
Muhammad Ahmad <daniyalabbas001@gmail.com>
2024-05-01 18:51:28 +0500

    Adding Tailwind and set login and signup page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -43,82 +43,48 @@ function Login() {
   // };
 
   return (
-    <div
-      style={{
-        maxWidth: "400px",
-        margin: "50px auto",
-        padding: "20px",
-        backgroundColor: "#fff",
-        borderRadius: "8px",
-        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <h2 style={{ textAlign: "center" }}>Login Form</h2>
-      <form onSubmit={onSubmit}>
-        <div style={{ marginBottom: "20px" }}>
-          <label
-            htmlFor="email"
-            style={{ fontWeight: "bold", display: "block" }}
-          >
-            Email:
-          </label>
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-2xl py-4 font-bold">Login Form</h1>
+      <form className="w-1/2 mx-auto" onSubmit={onSubmit}>
+        <div className="relative z-0 w-full mb-5 group">
           <input
             type="email"
-            id="email"
             name="email"
-            placeholder="Enter your email"
-            style={{
-              width: "100%",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              boxSizing: "border-box",
-            }}
+            id="floating_email"
             value={formData.email}
             onChange={handleChange}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
             required
           />
-        </div>
-        <div style={{ marginBottom: "20px" }}>
           <label
-            htmlFor="password"
-            style={{ fontWeight: "bold", display: "block" }}
+            htmlFor="floating_email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Password:
+            Email address
           </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
           <input
             type="password"
-            id="password"
             name="password"
-            placeholder="Enter your password"
-            style={{
-              width: "100%",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              boxSizing: "border-box",
-            }}
+            id="floating_password"
             value={formData.password}
             onChange={handleChange}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
             required
           />
+          <label
+            htmlFor="floating_password"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Password
+          </label>
         </div>
-        <input
-          type="submit"
-          value="Login"
-          style={{
-            width: "100%",
-            padding: "10px",
-            border: "none",
-            backgroundColor: "#007bff",
-            color: "#fff",
-            cursor: "pointer",
-            borderRadius: "5px",
-            transition: "background-color 0.3s",
-          }}
-        />
-        <Link to="/register">Click Here To Register </Link>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>
+      <Link to="/register" className="text-blue-700 border-b-2  border-blue-700"> Click Here to Register</Link>
     </div>
   );
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -5,21 +5,6 @@ import toast from "react-hot-toast";
 
 function Register() {
   const navigate = useNavigate();
-  const onSubmit = async (e, values) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post("/api/user/register", values);
-      if (response.data.success) {
-        toast.success(response.data.message, { duration: 1000 }); // Set duration to 1000 milliseconds (1 second)
-        toast.success("Redirecting to Login Page");
-        navigate("/login");
-      } else {
-        toast.error(response.data.message, { duration: 1000 }); // Set duration to 1000 milliseconds (1 second)
-      }
-    } catch (error) {
-      toast.error("Something went wrong", { duration: 1000 }); // Set duration to 1000 milliseconds (1 second)
-    }
-  };
 
   const [formData, setFormData] = useState({
     name: "",
@@ -35,108 +20,85 @@ function Register() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("/api/user/register", formData);
+      if (response.data.success) {
+        toast.success(response.data.message, { duration: 1000 });
+        toast.success("Redirecting to Login Page");
+        navigate("/login");
+      } else {
+        toast.error(response.data.message, { duration: 1000 });
+      }
+    } catch (error) {
+      toast.error("Something went wrong", { duration: 1000 });
+    }
+  };
+
   return (
-    <div
-      style={{
-        maxWidth: "400px",
-        margin: "50px auto",
-        padding: "20px",
-        backgroundColor: "#fff",
-        borderRadius: "8px",
-        boxShadow: "0 0 10px rgba(0, 0, 0, 0.1)",
-      }}
-    >
-      <h2 style={{ textAlign: "center" }}>Registration Form</h2>
-      <form onSubmit={(e) => onSubmit(e, formData)}>
-        <div style={{ marginBottom: "20px" }}>
-          <label
-            htmlFor="name"
-            style={{ fontWeight: "bold", display: "block" }}
-          >
-            Name:
-          </label>
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-2xl py-4 font-bold">Registration Form</h1>
+      <form className="w-1/2 mx-auto" onSubmit={onSubmit}>
+        <div className="relative z-0 w-full mb-5 group">
           <input
             type="text"
-            id="name"
             name="name"
-            placeholder="Enter your name"
-            style={{
-              width: "100%",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              boxSizing: "border-box",
-            }}
+            id="floating_first_name"
             value={formData.name}
             onChange={handleChange}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
             required
           />
-        </div>
-        <div style={{ marginBottom: "20px" }}>
           <label
-            htmlFor="email"
-            style={{ fontWeight: "bold", display: "block" }}
+            htmlFor="floating_first_name"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Email:
+            Full Name
           </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
           <input
             type="email"
-            id="email"
             name="email"
-            placeholder="Enter your email"
-            style={{
-              width: "100%",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              boxSizing: "border-box",
-            }}
+            id="floating_email"
             value={formData.email}
             onChange={handleChange}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
             required
           />
-        </div>
-        <div style={{ marginBottom: "20px" }}>
           <label
-            htmlFor="password"
-            style={{ fontWeight: "bold", display: "block" }}
+            htmlFor="floating_email"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
           >
-            Password:
+            Email address
           </label>
+        </div>
+        <div className="relative z-0 w-full mb-5 group">
           <input
             type="password"
-            id="password"
             name="password"
-            placeholder="Enter your password"
-            style={{
-              width: "100%",
-              padding: "10px",
-              borderRadius: "5px",
-              border: "1px solid #ccc",
-              boxSizing: "border-box",
-            }}
+            id="floating_password"
             value={formData.password}
             onChange={handleChange}
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            placeholder=" "
             required
           />
+          <label
+            htmlFor="floating_password"
+            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+          >
+            Password
+          </label>
         </div>
-        <input
-          type="submit"
-          value="Register"
-          style={{
-            width: "100%",
-            padding: "10px",
-            border: "none",
-            backgroundColor: "#007bff",
-            color: "#fff",
-            cursor: "pointer",
-            borderRadius: "5px",
-            transition: "background-color 0.3s",
-          }}
-        />
-        <Link to="/login">Click Here To Login </Link>
+        <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>
+      <Link to="/login" className="text-blue-700 border-b-2  border-blue-700"> Click Here to Login Page</Link>
     </div>
+
   );
 }
 
