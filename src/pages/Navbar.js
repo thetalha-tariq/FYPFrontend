3ddc93a019af83e75cc98629aeb2dc2dc0e1dfe1
Muhammad Ahmad <daniyalabbas001@gmail.com>
2024-05-13 22:42:13 +0500

    Set Product Page

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -1,36 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; 
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="col-span-12 md:col-span-10   overflow-auto bg-gray-100 navbar">
       <div className="brand">
         <span className="brand-text">PetMediConnect</span>
       </div>
       <ul className="nav-links">
         <li>
-          <Link to="/">Home</Link> 
+          <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link> 
+          <Link to="/about">About</Link>
         </li>
         <li>
-          <Link to="/services">Services</Link> 
+          <Link to="/services">Services</Link>
         </li>
         <li>
-          <Link to="/appointment">Make Appointment</Link> 
+          <Link to="/appointment">Make Appointment</Link>
         </li>
         <li>
-          <Link to="/product">Products</Link> 
+          <Link to="/product">Products</Link>
         </li>
         <li>
-          <Link to="/contact">Contact Us</Link> 
+          <Link to="/contact">Contact Us</Link>
         </li>
       </ul>
       <div className="login-container">
-      <Link to="/login" className="login-button">Login</Link> 
-  </div>
+        <Link to="/login" className="login-button">Login</Link>
+      </div>
     </nav>
   );
 };
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,44 +1,19 @@
 import React, { useState, useEffect } from "react";
-import axios from "../axiosInstance";
+import Products from "../Components/Products";
+
 
 const Product = () => {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get("/api/product/");
-        console.log(response.data);
-        setProducts(response.data.products);
-      } catch (error) {
-        console.error("Error fetching products:", error);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
   return (
-    
-    <div className="grid grid-cols-3 gap-4">
-      {products.map((product) => (
-        <div key={product._id} className="max-w-xs rounded overflow-hidden shadow-lg mt-20 mx-5">
-          <img className="w-full" src={product.image} alt={product.name} />
-          <div className="px-6 py-4">
-            <div className="font-bold text-xl mb-2">{product.name}</div>
-            <p className="text-gray-700 text-base">{product.description}</p>
-          </div>
-          <div className="px-6 pt-4 pb-2">
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-              {product.category}
-            </span>
-            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-              ${product.price}
-            </span>
-          </div>
-        </div>
-      ))}
+    <div>
+      <div className="flex flex-row flex-grow">
+        <li>All</li>
+        <li>Food</li>
+        <li>Accessories</li>
+        <li>Equipments</li>
+      </div>
+      {<Products />}
     </div>
+
   );
 };
 
